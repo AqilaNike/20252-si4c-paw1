b4137fe0ab2fbf546ebe0a349e477c70bb4b5201
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-03-31 13:14:03 +0700

    tambah gambar

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+//alert("Belajar Javascript");
+
+// ambil value nama dari pesan.html
+function kirim(){
+    let nama = document.getElementById('nama').value;
+console.log("Nama :" +nama)
+let saran = document.getElementById('saran').value;
+
+//tampilkan nama ke ul id-list-pesan
+//document.getElementById('list-pesan').innerHTML = `<li>${nama}</li>`;
+//document.getElementById('list-pesan').innerHTML += "<li>"+nama+"</li>";
+document.getElementById('list-pesan').innerHTML += `<tr><td>${nama}</td><td>${saran}</td><tr>`;
+}
